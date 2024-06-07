a3521e85d3238b0b5c30e99b8514bba5305355f0
Iya <danieliyapel2006@gmail.com>
2024-06-07 03:48:35 -0400

    Ejercicio 9
    Primero modelo funcional de esta forma de resolver esta lista con recursion

---
+++ ejercicios V6/9.RecursovoOrden.py
@@ -0,0 +1,34 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+    
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+    
+    # Combinar las dos mitades ordenadas
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    merged = []
+    left_index = right_index = 0
+    
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            merged.append(left[left_index])
+            left_index += 1
+        else:
+            merged.append(right[right_index])
+            right_index += 1
+    
+    merged += left[left_index:]
+    merged += right[right_index:]
+    
+    return merged
+
+arr = [3, 7, 2, 1, 6, 5, 4]
+sorted_arr = merge_sort(arr)
+print("Arreglo ordenado:", sorted_arr)
