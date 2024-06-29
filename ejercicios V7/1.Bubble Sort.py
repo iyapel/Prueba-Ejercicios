6de2a31ae58918b4e7121202d95d99eff267ad01
Iya <danieliyapel2006@gmail.com>
2024-06-28 21:23:12 -0400

    PARCHE 3.1

--- ejercicios V7/1.Bubble Sort.py
+++ ejercicios V7/1.Bubble Sort.py
@@ -1,17 +1,14 @@
-def bubble_sort(arr):
-    n = len(arr)
+def bubble_sort(arreglo):
+    n = len(arreglo)
     for i in range(n):
         swapped = False
         for x in range(0, n-i-1):
-            if arr[x] > arr[x+1]:
-                arr[x], arr[x+1] = arr[x+1], arr[x]
+            if arreglo[x] > arreglo[x+1]:
+                arreglo[x], arreglo[x+1] = arreglo[x+1], arreglo[x]
                 swapped = True
         if not swapped: break
-
-# Dada una lista desordenada                
+               
 x = [9, 8, 7, 5, 4, 3, 0]
 
-# La ordenamos utilizando bubble sort
 bubble_sort(x)
 print(x)
-# [0, 3, 4, 5, 7, 8, 9]
---
+++ ejercicios V7/2.Selection Sort.py
@@ -0,0 +1,17 @@
+def ordenamiento_por_seleccion(lista):
+    
+  for i in range(len(lista)):
+    indice_minimo = i
+    for x in range(i + 1, len(lista)):
+      if lista[x] < lista[indice_minimo]:
+        indice_minimo = x
+
+    if indice_minimo != i:
+      lista[i], lista[indice_minimo] = lista[indice_minimo], lista[i]
+
+  return lista
+
+lista_desordenada = [5, 2, 4, 1, 3]
+lista_ordenada = ordenamiento_por_seleccion(lista_desordenada)
+print(f"Lista desordenada: {lista_desordenada}")
+print(f"Lista ordenada: {lista_ordenada}")
