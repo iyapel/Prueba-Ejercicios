c5cf7a979c8bfbb99217ce359590ed1146fecc13
Iya <danieliyapel2006@gmail.com>
2024-07-18 20:47:48 -0400

    PARCHE 3.04

--- ejercicios V8/1.Nodos.py
+++ ejercicios V8/1.Nodos.py
@@ -4,4 +4,37 @@ class Nodo:
         self.left = None
         self.right = None
 
-#progreso xd
+class ABB:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        nuevo_nodo = Nodo(data)
+        if self.root is None:
+            self.root = nuevo_nodo
+        else:
+            actual = self.root
+            while True:
+                if data < actual.data:
+                    if actual.left is None:
+                        actual.left = nuevo_nodo
+                        break
+                    else:
+                        actual = actual.left
+                else:
+                    if actual.right is None:
+                        actual.right = nuevo_nodo
+                        break
+                    else:
+                        actual = actual.right
+
+    def search(self, data):
+        actual = self.root
+        while actual:
+            if data == actual.data:
+                return True
+            elif data < actual.data:
+                actual = actual.left
+            else:
+                actual = actual.right
+        return False
