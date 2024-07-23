c5ce0ea77c9c815b01bc8765342dc4e988d26996
Iya <danieliyapel2006@gmail.com>
2024-07-23 19:13:33 -0400

    PARCHE 3.0.7

--- ejercicios V8/2.Nodo en ABB.py
+++ ejercicios V8/2.Nodo en ABB.py
@@ -4,4 +4,19 @@ class Nodo:
     self.izquierdo = None
     self.derecho = None
 
+  def insertar(self, dato):
+    def insertar_recursivo(nodo, dato):
+      if nodo is None:
+        return Nodo(dato)
+      elif dato < nodo.dato:
+        nodo.izquierdo = insertar_recursivo(nodo.izquierdo, dato)
+      else:
+        nodo.derecho = insertar_recursivo(nodo.derecho, dato)
+      return nodo
 
+    self.raiz = insertar_recursivo(self.raiz, dato)
+
+abb = ABB()
+abb.insertar(10)
+abb.insertar(5)
+abb.insertar(15)
